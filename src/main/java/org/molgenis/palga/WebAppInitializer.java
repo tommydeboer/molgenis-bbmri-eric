3d582303d8d40da9100ceef14bc37ac0ad074a3b
jjettenn <jjettenn@gmail.com>
2014-04-17 13:08:16 +0200

    WebAppInitializer extends MolgenisWebAppInitializer

--- src/main/java/org/molgenis/palga/WebAppInitializer.java
+++ src/main/java/org/molgenis/palga/WebAppInitializer.java
@@ -13,13 +13,14 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
-public class WebAppInitializer implements WebApplicationInitializer
+public class WebAppInitializer implements WebApplicationInitializer // extends MolgenisWebAppInitializer
 {
 	private static final Logger logger = Logger.getLogger(WebAppInitializer.class);
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException
 	{
+		// super.onStartup TODO JJ
 		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
 		ctx.register(WebAppConfig.class);
 
